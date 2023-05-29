cb4a8f8a96bb9161811368f67bfd76455c6107da
Yoshikawa290036 <290036hirokix@gmail.com>
2023-05-29 16:32:08 +0900

    create repository

---
+++ config/atcoder.sh
@@ -0,0 +1,16 @@
+cp /mnt/c/AtCoder/config/cppform.cpp   cpp_A.cpp
+cp /mnt/c/AtCoder/config/cppform.cpp   cpp_B.cpp
+cp /mnt/c/AtCoder/config/cppform.cpp   cpp_C.cpp
+cp /mnt/c/AtCoder/config/cppform.cpp   cpp_D.cpp
+cp /mnt/c/AtCoder/config/cppform.cpp   cpp_E.cpp
+cp /mnt/c/AtCoder/config/cppform.cpp   cpp_F.cpp
+cp /mnt/c/AtCoder/config/cppform.cpp   cpp_test.cpp
+
+
+cp /mnt/c/AtCoder/config/pyform.py  py_A.py
+cp /mnt/c/AtCoder/config/pyform.py  py_B.py
+cp /mnt/c/AtCoder/config/pyform.py  py_C.py
+cp /mnt/c/AtCoder/config/pyform.py  py_D.py
+cp /mnt/c/AtCoder/config/pyform.py  py_E.py
+cp /mnt/c/AtCoder/config/pyform.py  py_F.py
+cp /mnt/c/AtCoder/config/pyform.py  py_test.py
